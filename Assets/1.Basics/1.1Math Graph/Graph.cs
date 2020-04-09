723275c1f781b0718c6f1a2c5c163e77fd22eeb8
Yzngo <yzngo@live.com>
2020-04-09 11:55:58 +0800

    Mathematical Surfaces 3.Adding Another Dimension

--- Assets/1.Basics/1.1Math Graph/Graph.cs
+++ Assets/1.Basics/1.1Math Graph/Graph.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
+    public GraphFunctionName functionName;
     [SerializeField] private Transform pointPrefab;
 
     [Range(10,1000)]
@@ -15,6 +16,8 @@ public class Graph : MonoBehaviour
     List<Transform> _points = new List<Transform>();
     private int curResolusion = 0;
     private int curXRange = 0;
+
+    static private GraphFunction[] functions = {SineFunction, MultiSineFunction};
     private void Update() {
         SpawnPoints();
         UpdateGraph();
@@ -49,16 +52,37 @@ public class Graph : MonoBehaviour
     
     private void UpdateGraph()
     {
+        GraphFunction f = functions[(int)functionName];
+        var time = Time.time;
         for(int i = 0; i < resolusion; i++) {
             Transform point = _points[i];
             Vector3 position = point.localPosition;
-            position.y = CalcX(position.x);
+
+            position.y = f(position.x, time);
+            // position.y = MultiSineFunction(position.x, time);
+
             point.localPosition = position;
         }
     }
 
-    private float CalcX(float x)
+    //Although the SineFunction and MultiSineFunction are part of Graph, 
+    //they are effectively self-contained.
+
+    //Because static methods aren't associated with object instances, 
+    //the compiled code doesn't have to keep track of 
+    //which object you're invoking the method on. 
+    //This means that static method invocations are a bit faster, 
+    //but it's usually not significant enough to worry about.
+    static float SineFunction(float x, float param)
     {
-        return Mathf.Sin(Mathf.PI *(x + Time.time));
+        return Mathf.Sin(Mathf.PI *(x + param));
     }
+
+    static float MultiSineFunction(float x, float param) 
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + param));
+        y += Mathf.Sin(2f * Mathf.PI * (x + param)) / 2f;
+        return y;
+    }
+
 }
---
+++ Assets/1.Basics/1.1Math Graph/GraphFunction.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//定义委托 -> 方法的签名
+public delegate float GraphFunction(float x, float y);
