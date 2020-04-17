08726665606267ec57d37a51fd7f4bf869094394
Yzngo <yzngo@live.com>
2020-04-17 09:34:37 +0800

    Building an Atomic Nucleus

---
+++ Assets/1.Basics/1.3AtomicNucleus/NucleonSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
