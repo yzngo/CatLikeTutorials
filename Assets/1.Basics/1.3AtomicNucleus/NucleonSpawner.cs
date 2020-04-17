fb041241e707ab12c3f39b0506821b8c2785dea2
Yzngo <yzngo@live.com>
2020-04-17 14:09:14 +0800

    update

--- Assets/1.Basics/1.3AtomicNucleus/NucleonSpawner.cs
+++ Assets/1.Basics/1.3AtomicNucleus/NucleonSpawner.cs
@@ -7,15 +7,20 @@ public class NucleonSpawner : MonoBehaviour
     public float timeBetweenSpawns;
     public float spawnDistance;
     public Nucleon[] nucleonPrefabs;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private float timeSinceLastSpawn;
+
+    private void FixedUpdate() {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns) {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void SpawnNucleon() {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
     }
 }
