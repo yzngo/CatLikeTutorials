fb0b53145daaa50ea3eae1a86cff02bc6a703eab
Yzngo <yzngo@live.com>
2020-04-18 16:53:06 +0800

    sliding a sphere done

---
+++ Assets/2.Movement/2.1Sliding a Sphere/MyControls.cs
@@ -0,0 +1,220 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/2.Movement/2.1Sliding a Sphere/MyControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @MyControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @MyControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""MyControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""8a477225-4882-4e84-935d-2a76d6b0302b"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""ed1286f0-d25e-486e-bb72-3a0836a49431"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": ""NormalizeVector2"",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b7cc845a-0f43-424e-ab29-f5b9138cf034"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ba284cd4-898f-4c59-b6e4-3b7e925409cb"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0569c2a1-8a32-4b06-89a5-c7dba34be85b"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""11deeaf0-240d-4ae2-b21e-48f42e3ca9b4"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""68ffb2f8-27f3-4554-864f-2ddedda963a2"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""4597358d-1104-48fe-8cba-d9657f2b8dd7"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""cb9fedb2-9c1a-4eca-aa5a-7f16614a6d2a"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0ff1d236-97e8-4065-8d98-ad74616aa17f"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""2ba5eab9-5a88-46d7-bcd4-2a507cf539f9"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    public struct PlayerActions
+    {
+        private @MyControls m_Wrapper;
+        public PlayerActions(@MyControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
--- Assets/2.Movement/2.1Sliding a Sphere/Scripts/MovingSphere.cs
+++ Assets/2.Movement/2.1Sliding a Sphere/Scripts/MovingSphere.cs
@@ -1,7 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovingSphere : MonoBehaviour
 {
+    // public MyControls inputActions;
+
+    [SerializeField]
+    private Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    [SerializeField, Range(0f, 100f)]
+    private float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    private float maxAcceleration = 10f;
+
+    [SerializeField, Range(0f, 1f)]
+    private float bounciness = 0.5f;
+
+    private Vector3 velocity;
+    private void Awake() {
+        // inputActions = new MyControls();
+        // inputActions.Enable();
+        // inputActions.Player.Move.performed += ctx =>
+        // {
+        //     Move(ctx.ReadValue<Vector2>());
+        // };
+    }
+
+    private void OnDestroy() {
+        // inputActions.Disable();
+    }
+
+    private void Move(Vector2 input) {
+        Vector3 desireVelocity = new Vector3(input.x, 0f, input.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        // if (velocity.x < desireVelocity.x) {
+        //     velocity.x = Mathf.Min(velocity.x + maxSpeedChange, desireVelocity.x);
+        // } else {
+        //     velocity.x = Mathf.Max(velocity.x - maxSpeedChange, desireVelocity.x);
+        // }
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desireVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desireVelocity.z, maxSpeedChange);
+        
+        // Vector3 acceleration = new Vector3(input.x, 0f, input.y) * maxSpeed;
+        // velocity += acceleration * Time.deltaTime;
+        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+        // if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z))) {
+        //     newPosition.x = Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
+        //     newPosition.z = Mathf.Clamp(newPosition.z, allowedArea.yMin, allowedArea.yMax);
+        //     newPosition = transform.localPosition;
+        // }
+        if (newPosition.x < allowedArea.xMin) {
+            newPosition.x = allowedArea.xMin;
+            velocity.x = -velocity.x * bounciness;
+        }
+        else if (newPosition.x > allowedArea.xMax) {
+            newPosition.x = allowedArea.xMax;
+            velocity.x = -velocity.x * bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin) {
+            newPosition.z = allowedArea.yMin;
+            velocity.z = -velocity.z * bounciness;
+        }
+        else if(newPosition.z > allowedArea.xMax) {
+            newPosition.z = allowedArea.xMax;
+            velocity.z = -velocity.z * bounciness;
+        }
+
+        transform.localPosition = newPosition;
+        // transform.Translate(new Vector3(move.x, 0f, move.y));
+        // transform.localPosition = new Vector3(move.x, 0f, move.y);
+    }
+    private void Update() {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        // playerInput.Normalize();
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Move(playerInput);
+    }
 }
